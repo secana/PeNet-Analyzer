5cee4e7aad64757298f604bb703b9d5f53e3e5ca
Stefan Hausotte <stefan.hausotte@gdata.de>
2020-03-13 16:09:36 +0100

    Fix all tests

--- src/PeNet.Analyzer/Anomalies/InvalidResourceDirectory.cs
+++ src/PeNet.Analyzer/Anomalies/InvalidResourceDirectory.cs
@@ -4,23 +4,41 @@ namespace PeNet.Analyzer.Anomalies
 {
     public class InvalidResourceDirectory : Anomaly
     {
-        public InvalidResourceDirectory(PeFile peFile) 
+        public InvalidResourceDirectory(PeFile peFile)
             : base(peFile,
-                "Invalid Resource directory.", 
-                RuntimeBehavior.NotRunnable) { }
+                "Invalid Resource directory.",
+                RuntimeBehavior.NotRunnable)
+        { }
 
         protected override bool MatchAnomaly(PeFile peFile)
         {
-            bool InValidResDir(PeFile peFile)
-            {
-                return peFile.ImageResourceDirectory == null;
-            }
 
-            return InValidResDir(peFile)
-                && peFile
-                    ?.ImageNtHeaders
-                    ?.OptionalHeader
-                    .DataDirectory[(int)DataDirectoryType.Resource].Size != 0;
+            if (peFile
+                .ImageNtHeaders
+                ?.OptionalHeader
+                .DataDirectory[(int) DataDirectoryType.Resource].VirtualAddress == 0)
+                return false;
+
+            if (peFile.ImageResourceDirectory == null)
+                return true;
+
+            if (peFile
+                .ImageNtHeaders
+                ?.OptionalHeader
+                .DataDirectory[(int)DataDirectoryType.Resource].Size == 0)
+                return true;
+
+            if (peFile.ImageResourceDirectory.NumberOfIdEntries +
+                peFile.ImageResourceDirectory.NumberOfNameEntries > 0
+                && peFile.ImageResourceDirectory.DirectoryEntries == null)
+                return true;
+
+            if (peFile.ImageResourceDirectory.NumberOfIdEntries +
+                peFile.ImageResourceDirectory.NumberOfNameEntries >
+                peFile.ImageResourceDirectory.DirectoryEntries?.Count)
+                return true;
+
+            return false;
         }
     }
 }
