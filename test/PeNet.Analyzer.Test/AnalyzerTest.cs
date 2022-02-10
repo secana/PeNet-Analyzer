7aec57f6fde8a4bc883a74fb4e06f2ab301e39e8
Stefan Hausotte <stefan.hausotte@gdata.de>
2022-02-10 12:24:51 +0100

    update to run tests on linux

--- test/PeNet.Analyzer.Test/AnalyzerTest.cs
+++ test/PeNet.Analyzer.Test/AnalyzerTest.cs
@@ -5,10 +5,10 @@ namespace PeNet.Analyzer.Test
     public class AnalyzerTest
     {
         [Theory]
-        [InlineData(@".\Binaries\0389f753504c2aac14dd947155f2baa8cff6381ae124fcbc6cf98710271738ac", 0)]
-        [InlineData(@".\Binaries\4df983666111ccfd9b4e9f5e304ae3ad9728f7d8c99ddc71bfa539eb342be13c", 2)]
-        [InlineData(@".\Binaries\1daf18671e42c6550b7944360a95e65426475559a863a8140162c6bcda6728ee", 1)]
-        [InlineData(@".\Binaries\3426efcbfe65a596accfd3296f74a1fc146e8afeae9c4a89cd7a452cf46ba98c", 4)]
+        [InlineData(@"./Binaries/0389f753504c2aac14dd947155f2baa8cff6381ae124fcbc6cf98710271738ac", 0)]
+        [InlineData(@"./Binaries/4df983666111ccfd9b4e9f5e304ae3ad9728f7d8c99ddc71bfa539eb342be13c", 2)]
+        [InlineData(@"./Binaries/1daf18671e42c6550b7944360a95e65426475559a863a8140162c6bcda6728ee", 1)]
+        [InlineData(@"./Binaries/3426efcbfe65a596accfd3296f74a1fc146e8afeae9c4a89cd7a452cf46ba98c", 4)]
         public void MatchAnomalies(string file, int numOfMatches)
         {
             var peFile = new PeFile(file);
--- test/PeNet.Analyzer.Test/Anomalies/NumOfRvaAndSizesSmallerThanNumOfDataDirsTest.cs
+++ test/PeNet.Analyzer.Test/Anomalies/NumOfRvaAndSizesSmallerThanNumOfDataDirsTest.cs
@@ -6,9 +6,9 @@ namespace PeNet.Analyzer.Test.Anomalies
     public class NumOfRvaAndSizesSmallerThanNumOfDataDirsTest
     {
         [Theory]
-        [InlineData(@".\Binaries\78e859b5bc8d9adf4635a12f405710a0b9710e09278bc1453ca6dd5413aeff1c", true)]
-        [InlineData(@".\Binaries\0ba44e3222d1034cc2b16cc7625366777507e96ee7b988589fff9b82851c7655", false)]
-        [InlineData(@".\Binaries\1daf18671e42c6550b7944360a95e65426475559a863a8140162c6bcda6728ee", false)]
+        [InlineData(@"./Binaries/78e859b5bc8d9adf4635a12f405710a0b9710e09278bc1453ca6dd5413aeff1c", true)]
+        [InlineData(@"./Binaries/0ba44e3222d1034cc2b16cc7625366777507e96ee7b988589fff9b82851c7655", false)]
+        [InlineData(@"./Binaries/1daf18671e42c6550b7944360a95e65426475559a863a8140162c6bcda6728ee", false)]
         public void NumberOfRvaAndSizesSmallerThanNumOfDataDirs(string file, bool isValid)
         {
             var peFile = new PeFile(file);
